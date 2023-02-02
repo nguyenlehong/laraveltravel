7dfa023579fbe53865b2690735f6e0de04bf0487
nguyenle <lhnguyen0401@gmail.com>
2023-02-03 01:10:55 +0700

    css home user

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -4,74 +4,79 @@
 @include('frontend.head')
 
 <body>
-    @include('frontend.nav')
+  @include('frontend.nav')
 
-    <section class="home">
-        <h2>Tuyến phổ biến</h2>
-        <div class="home-container">
-            
-            <div class="home-box">
-                <div class="card mb-3">
-                    <div class="row g-0">
-                      <div class="col-md-4">
-                        <img src="..." class="img-fluid rounded-start" alt="...">
-                      </div>
-                      <div class="col-md-8">
-                        <div class="card-body">
-                          <h5 class="card-title">Sài Gòn => Đà lạt</h5>
-                          <p class="card-text"> 100km - 2h - 300.000đ</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+
+  <section class="home">
+
+    <div class="home-container">
+
+      <div class="home-box">
+        <div class="card mb-3 home-card rounded-start">
+
+          <div class="row g-0">
+            <div class="col-md-4 home-date rounded-start ">
+              <h3>2023-02-12</h3>
             </div>
-            <div class="home-box">
-                <div class="card mb-3">
-                    <div class="row g-0">
-                      <div class="col-md-4">
-                        <img src="..." class="img-fluid rounded-start" alt="...">
-                      </div>
-                      <div class="col-md-8">
-                        <div class="card-body">
-                          <h5 class="card-title">Sài Gòn => Đà lạt</h5>
-                          <p class="card-text"> 100km - 2h - 300.000đ</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+            <div class="col-md-8 home-text">
+              <a href="">
+                <div class="card-body ps-5">
+                  <h5 class="card-title">Sài Gòn => Đà lạt </h5>
+                  <p class="card-text"> 100km - 2h - 300.000đ</p>
+                </div>
+              </a>
             </div>
-            <div class="home-box">
-                <div class="card mb-3">
-                    <div class="row g-0">
-                      <div class="col-md-4">
-                        <img src="..." class="img-fluid rounded-start" alt="...">
-                      </div>
-                      <div class="col-md-8">
-                        <div class="card-body">
-                          <h5 class="card-title">Sài Gòn => Đà lạt</h5>
-                          <p class="card-text"> 100km - 2h - 300.000đ</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-            </div>
-            <div class="home-box">
-                <div class="card mb-3">
-                    <div class="row g-0">
-                      <div class="col-md-4">
-                        <img src="..." class="img-fluid rounded-start" alt="...">
-                      </div>
-                      <div class="col-md-8">
-                        <div class="card-body">
-                          <h5 class="card-title">Sài Gòn => Đà lạt</h5>
-                          <p class="card-text"> 100km - 2h - 300.000đ</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-            </div>
+          </div>
+
         </div>
-    </section>
+      </div>
+      <div class="home-box">
+        <div class="card mb-3 home-card rounded-start">
+          <div class="row g-0">
+            <div class="col-md-4 home-date rounded-start ">
+              <h3>2023-02-12</h3>
+            </div>
+            <div class="col-md-8">
+              <div class="card-body ps-5">
+                <h5 class="card-title">Sài Gòn => Đà lạt </h5>
+                <p class="card-text"> 100km - 2h - 300.000đ</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="home-box">
+        <div class="card mb-3 home-card rounded-start">
+          <div class="row g-0">
+            <div class="col-md-4 home-date rounded-start ">
+              <h3>2023-02-12</h3>
+            </div>
+            <div class="col-md-8">
+              <div class="card-body ps-5">
+                <h5 class="card-title">Sài Gòn => Đà lạt </h5>
+                <p class="card-text"> 100km - 2h - 300.000đ</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="home-box">
+        <div class="card mb-3 home-card rounded-start">
+          <div class="row g-0">
+            <div class="col-md-4 home-date rounded-start ">
+              <h3>2023-02-12</h3>
+            </div>
+            <div class="col-md-8">
+              <div class="card-body ps-5">
+                <h5 class="card-title">Sài Gòn => Đà lạt </h5>
+                <p class="card-text"> 100km - 2h - 300.000đ</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </body>
 
 </html>
